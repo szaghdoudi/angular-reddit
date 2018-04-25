d2c72fce578a737764a4eb605f8d995830b93187
szaghdoudi <sofian.zaghdoudi@gmail.com>
2018-04-25 23:28:46 +0200

    ng-book2-p48

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  @HostBinding('attr.class') cssClass = 'row';
+  article : Article;
+
+
+  constructor() {
+
+    this.article = new Article('Angular 5','http://angular.io',10);
+   
+  }
+
+  voteUp() {
+    this.article.votes += 1;
+    return false;
+  }
+
+  voteDown() {
+    this.article.votes -= 1;
+    return false;
+  }
+
+  ngOnInit() {
+  }
+
+}
